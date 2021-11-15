bc7c978cd28f64632809553799e504b8e74df181
Eugene Kravchuk <gene.int32@gmail.com>
2021-11-15 23:40:26 +0200

    [home] Init Home screen

---
+++ src/home/Home.tsx
@@ -0,0 +1,13 @@
+import Reactfrom from 'react';
+
+import './Home.scss';
+
+const Home = () => {
+  return (
+    <div className="home">
+      <div className="home__headline headline-4">Awesome React Apps</div>
+    </div>
+  );
+};
+
+export default Home;
