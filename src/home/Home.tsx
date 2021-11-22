9a9a78382eb7fcdafad8d4f0f66d02f724510ae2
Eugene Kravchuk <gene.int32@gmail.com>
2021-11-22 23:34:50 +0200

    [home] Implement Home page

--- src/home/Home.tsx
+++ src/home/Home.tsx
@@ -1,12 +1,51 @@
-import React from 'react';
+import React, {Suspense, useState, useEffect, lazy, LazyExoticComponent, ComponentType} from 'react';
+import {Routes, Route, Link, Outlet} from 'react-router-dom';
 
+import data from '../app-data.json';
+import {AppData} from '../types';
 import './Home.scss';
+import AppCard from './components/app-card/AppCard';
+import AppList from './components/app-list/AppList';
+
+const rawAppData: AppData[] = data;
+
+const lazyRoutes: {
+  LazyComponent: LazyExoticComponent<ComponentType<unknown>>;
+  location: string;
+}[] = [];
+
+for (const app of rawAppData) {
+  const {componentName, dirName, location} = app;
+
+  lazyRoutes.push({
+    LazyComponent: lazy(() => import(`../${dirName}/${componentName}`)),
+    location,
+  });
+}
 
 const Home = () => {
+  const [appData, setAppData] = useState<AppData[]>([]);
+
+  useEffect(() => {
+    setAppData(rawAppData);
+  }, []);
+
   return (
-    <div className="home">
-      <div className="home__headline headline-4">Awesome React Apps</div>
-    </div>
+    <Suspense fallback={<></>}>
+      <Routes>
+        {lazyRoutes.map(({location, LazyComponent}, key) => (
+          <Route key={key} path={location} element={<LazyComponent />}></Route>
+        ))}
+      </Routes>
+      <div className="home">
+        <div className="home__headline headline-4">Awesome React Apps</div>
+        <AppList>
+          {appData.map((app, i) => (
+            <AppCard key={i} {...app} />
+          ))}
+        </AppList>
+      </div>
+    </Suspense>
   );
 };
 
